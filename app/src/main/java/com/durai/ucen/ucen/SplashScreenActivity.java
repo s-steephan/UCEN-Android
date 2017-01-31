25b3130b8609f223cf99e5fa9eb9b8a7b3247ef0
s-steephan <selvasteephan@gmail.com>
2017-01-31 15:44:31 +0530

    Add splashscreen and associated libs, activity and layout
    New splashscreen has been added

---
+++ app/src/main/java/com/durai/ucen/ucen/SplashScreenActivity.java
@@ -0,0 +1,27 @@
+package com.durai.ucen.ucen;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends Activity {
+
+    private static final int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
