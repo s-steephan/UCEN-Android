da47831ef8cc811c367bc9e9e75178069f9187a8
s-steephan <selvasteephan@gmail.com>
2017-02-04 23:30:35 +0530

    Change login logic
    User will see main activity if they already did login

--- app/src/main/java/com/durai/ucen/ucen/SplashScreenActivity.java
+++ app/src/main/java/com/durai/ucen/ucen/SplashScreenActivity.java
@@ -2,23 +2,35 @@ package com.durai.ucen.ucen;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class SplashScreenActivity extends Activity {
 
     private static final int SPLASH_TIME_OUT = 3000;
+    public static final String PREFS_NAME = "Login_Token";
+    String token;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        SharedPreferences settings = SplashScreenActivity.this.getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        token = settings.getString("token", "");
         new Handler().postDelayed(new Runnable() {
 
             @Override
             public void run() {
-                Intent i = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                Intent i;
+                if (token!="") {
+                    i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                }
+                else {
+                    i = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                }
                 startActivity(i);
                 finish();
             }
