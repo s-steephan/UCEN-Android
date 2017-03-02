5626f31125ca47e31acff4d09c4c8f9ae25911d6
s-steephan <selvasteephan@gmail.com>
2017-03-02 19:56:27 +0530

    Add locate us, feedback features
    Add map intent, email intent to bring map, feedback features

--- app/src/main/java/com/durai/ucen/ucen/MainActivity.java
+++ app/src/main/java/com/durai/ucen/ucen/MainActivity.java
@@ -3,6 +3,7 @@ package com.durai.ucen.ucen;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -239,9 +240,26 @@ public class MainActivity extends AppCompatActivity
             startActivity(new Intent(MainActivity.this, AttendanceActivity.class));
         } else if (id == R.id.nav_result) {
             startActivity(new Intent(MainActivity.this, SemestersListActivity.class));
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_map) {
+            String lat="8.167338", lng="77.413882", mTitle="University College of Engineering, Nagercoil";
+            String geoUri = "http://maps.google.com/maps?q=loc:" + lat + "," + lng + " (" + mTitle + ")";
+            Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
+                    Uri.parse(geoUri));
+            intent.setClassName("com.google.android.apps.maps",
+                    "com.google.android.maps.MapsActivity");
+            startActivity(intent);
+            /*Intent i = new Intent(Intent.ACTION_VIEW,Uri.parse("geo:8.167338, 77.413882"));
+            i.setClassName("com.google.android.apps.maps",
+                    "com.google.android.maps.MapsActivity");
+            startActivity(i);*/
 
         } else if (id == R.id.nav_send) {
+            String mailId = "selvaduraimurugan@gmail.com";
+            Intent emailIntent = new Intent(Intent.ACTION_SENDTO,
+                    Uri.fromParts("mailto", mailId, null));
+            emailIntent.setClassName("com.google.android.gm", "com.google.android.gm.ComposeActivityGmail");
+            emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Feedback - Reg");
+            startActivity(Intent.createChooser(emailIntent, "Email via"));
 
         } else if (id == R.id.nav_send) {
 
